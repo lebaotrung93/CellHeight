d82405e067098e305c4c3cc78b2ccf657179311e
Le Bao Trung (RBVH/ETM2) <Trung.LeBao@vn.bosch.com>
2020-06-19 10:05:49 +0700

    animation
    hmmm..

--- CellHeight/Cell.swift
+++ CellHeight/Cell.swift
@@ -2,13 +2,23 @@ import UIKit
 
 class Cell: UICollectionViewCell {
   @IBOutlet private var detailsView: UIView!
-
+    private var _isOpsen : Bool!
   var resize: (() -> Void)?
 
   override func awakeFromNib() {
     super.awakeFromNib()
     contentView.layer.cornerRadius = 10
   }
+    
+    var isOpen : Bool {
+        set {
+            detailsView.isHidden = !newValue
+            _isOpsen = newValue
+        }
+        get {
+            return _isOpsen
+        }
+    }
 
   @IBAction private func toggleDetails() {
     detailsView.isHidden.toggle()
--- CellHeight/ViewController.swift
+++ CellHeight/ViewController.swift
@@ -2,10 +2,12 @@ import UIKit
 
 class ViewController: UIViewController {
   @IBOutlet private var collectionView: UICollectionView!
-
+    var data = Array(repeating: false, count: 10)
   override func viewDidLoad() {
     super.viewDidLoad()
-    collectionView.collectionViewLayout = createLayout()
+    collectionView.delegate = self
+    collectionView.dataSource = self
+//    collectionView.collectionViewLayout = createLayout()
   }
 
   private func createLayout() -> UICollectionViewLayout {
@@ -26,16 +28,22 @@ class ViewController: UIViewController {
 
 extension ViewController: UICollectionViewDataSource {
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return 10
+    return data.count
   }
 
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! Cell
+    cell.isOpen = data[indexPath.row]
     cell.resize = { [weak self] in
-      UIView.animate(withDuration: 2) {
-        self?.collectionView.collectionViewLayout.invalidateLayout()
-      }
+        self?.data[indexPath.row] = !(self?.data[indexPath.row] ?? false)
+        self?.collectionView.reloadItems(at: [indexPath])
     }
     return cell
   }
+}
+
+extension ViewController : UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return data[indexPath.row] ? CGSize(width: collectionView.bounds.width, height: 200) : CGSize(width: collectionView.bounds.width, height: 60)
+    }
 }
